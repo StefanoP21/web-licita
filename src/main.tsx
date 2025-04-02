003fe70ab0b8f10938d1280b48dc35faf5bb3471
Stefano Palomino <stefanop21@outlook.es>
2025-04-02 00:18:30 -0500

    feat(plugins): added tan stack provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
 import { BrowserRouter } from 'react-router';
+import { TanStackProvider } from './plugins/TanStackProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <TanStackProvider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </TanStackProvider>
   </StrictMode>
 );
